9d969d786f638718d8a4f2dd7e3a3270a56e5a00
leo <magicsm23@gmail.com>
2026-05-15 13:48:54 +0900

    Updates

---
+++ _next/static/8Cf7o1-UZngeP97B1Fnds/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/898-b1ea4411a568f393.js","static/chunks/pages/index-49a84e13890f1854.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
