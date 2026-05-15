7550c54c630d54889cb83df203f100638831545d
leo <magicsm23@gmail.com>
2026-05-15 13:53:20 +0900

    Updates

--- _next/static/2Yu-Fmmy0io49X6gl13OH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/898-b1ea4411a568f393.js","static/chunks/pages/index-275361f9c5eb31eb.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/HTTR7M-xfX7-aiK9iQhVT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/898-b1ea4411a568f393.js","static/chunks/pages/index-5d63f1ee17a4e997.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
